0069009bdfe439a9bb7e0c19c68200e170d21fba
devansh5 <devkumar5436@gmail.com>
2020-04-26 04:30:56 +0530

    static path updated

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -128,13 +128,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
-]
+
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
