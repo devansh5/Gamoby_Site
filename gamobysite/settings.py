c1b0d26548f5d0abfebfa96c901a29fef5983366
devansh5 <devkumar5436@gmail.com>
2020-04-26 06:51:31 +0530

    dbte

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -12,11 +12,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import django_heroku
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
