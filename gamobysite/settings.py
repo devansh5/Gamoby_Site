1057035e8dd45532e23a675eabb7f219d0cbe7d9
devansh5 <devkumar5436@gmail.com>
2020-04-26 09:35:26 +0530

    updated ui

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -132,17 +132,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-
 MEDIA_URL = '/images/'
-
-
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
