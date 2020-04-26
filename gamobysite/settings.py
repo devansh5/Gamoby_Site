5cd49d575fc57ad4e2251a739df3dc83e4f9f45f
devansh5 <devkumar5436@gmail.com>
2020-04-26 10:06:06 +0530

    base dir

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -132,6 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
