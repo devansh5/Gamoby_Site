076bc16b38cbaca50c6d6b34719787bac6c96852
devansh5 <devkumar5436@gmail.com>
2020-04-26 10:34:27 +0530

    stadirs

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -138,6 +138,10 @@ STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/images/'
