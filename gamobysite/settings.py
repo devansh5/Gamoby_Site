316be0fac9218a249e80ab0853e2e12da8e65d1d
devansh5 <devkumar5436@gmail.com>
2020-04-26 06:04:29 +0530

    dbtesting

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -92,6 +93,11 @@ WSGI_APPLICATION = 'gamobysite.wsgi.application'
 #     }
 # }
 
+DATABASES = {
+    'default': {
+        'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+}
 
 
 # Password validation
