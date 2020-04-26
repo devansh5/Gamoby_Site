ca2b55c82a57a875fee1f76da001d635792fae95
devansh5 <devkumar5436@gmail.com>
2020-04-26 05:50:05 +0530

    django-heroku added

--- gamobysite/settings.py
+++ gamobysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,3 +174,6 @@ MEDIA_ROOT='media'
 MEDIA_URL = URL + 'media/'
 
 DEFAULT_FILE_STORAGE = 'gamobysite.storage_backends.MediaStorage'
+
+
+django_heroku.settings(locals())
